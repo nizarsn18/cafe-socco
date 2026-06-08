de0258eba7fecc19da2e8d197d64c7261a11f41a
nizarsn18 <117095850+nizarsn18@users.noreply.github.com>
2026-06-08 06:47:33 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuration Café Socco
+ * Pour envoi email automatique : créez une clé gratuite sur https://web3forms.com
+ * (entrez Jihanemj111@gmail.com, copiez la clé ci-dessous)
+ */
+window.CAFE_SOCCO_CONFIG = {
+  email: 'Jihanemj111@gmail.com',
+  whatsapp: '212777498828',
+  web3formsKey: '',
+};
